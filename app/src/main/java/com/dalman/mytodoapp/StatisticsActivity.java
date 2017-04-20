6ec0884f8637f1068d803fdf6a7a6719615af0b4
peyvandgolestani <pey1673@gmail.com>
2017-04-20 16:04:32 +0200

    redigering task

--- app/src/main/java/com/dalman/mytodoapp/StatisticsActivity.java
+++ app/src/main/java/com/dalman/mytodoapp/StatisticsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class StatisticsActivity extends AppCompatActivity {
 
@@ -13,10 +14,20 @@ public class StatisticsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
         ArrayList<Task>  tasks = getIntent().getParcelableArrayListExtra(TaskListActivity.KEY_TASKS);
+        Date youngest = new Date(0);
+        Date oldest = new Date(Long.MAX_VALUE);
+
         int total = tasks.size();
         int completed = 0;
         int archived = 0;
         for (Task task : tasks) {
+            Date started = task.getStarted();
+            if (started.after(youngest)) {
+                youngest = started;
+            }
+            if (started.before(oldest)){
+                oldest = started;
+            }
             if (task.isCompleted()){
                 completed++;
             }
@@ -28,9 +39,14 @@ public class StatisticsActivity extends AppCompatActivity {
         tasksTotal.setText(String.valueOf(total));
         TextView tasksCompleted = (TextView) findViewById(R.id.tasks_completed);
         tasksCompleted.setText(String.valueOf(completed));
-        TextView tasksArchived = (TextView) findViewById(R.id.tasks_archived);
+        TextView tasksArchived = (TextView) findViewById(R.id.average_per_day);
         tasksArchived.setText((String.valueOf(archived)));
 
+        double timeDiff = youngest.getTime() - oldest.getTime();
+        double days = (timeDiff / (1000 * 60 * 24)) +1;
+
+        TextView  avgPerDay = (TextView) findViewById(R.id.average_per_day);
+        avgPerDay.setText(String.valueOf(total / days));
 
     }
 
